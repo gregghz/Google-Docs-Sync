29cf5936e97acc5a181786be059077baa1cae6ca
Greggory Hernandez <greggory.hz@gmail.com>
2011-06-19 14:17:21 -0600

    more commits I left out

--- daemon.py
+++ daemon.py
@@ -1,4 +1,4 @@
-import os, sys, atexit
+import os, sys, atexit, signal, time
 
 class Daemon(object):
     """
@@ -101,7 +101,7 @@ class Daemon(object):
         # Try killing the daemon process
         try:
             while 1:
-                os.kill(pid, SIGTERM)
+                os.kill(pid, signal.SIGTERM)
                 time.sleep(0.1)
         except OSError, err:
             err = str(err)
